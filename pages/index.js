c06dea7c1eae2acea44a4b8fa60fb76ae02b8f1b
Brandon Arbini <b@sevenwire.com>
2019-11-15 16:25:56 -0800

    Initial commit.

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../components/layout";
+import "./index.scss";
+
+const Home = () => (
+  <Layout className="home">
+    <h3>Opening January 2020</h3>
+    <p>
+      <address>
+        150 Paularino Ave, Bldg B<br />
+        Costa Mesa, CA 92626
+      </address>
+    </p>
+  </Layout>
+);
+
+export default Home;
